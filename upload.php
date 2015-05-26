6e9f5d0662a9bd0130380450ded684e30e0e3aaa
KEVIN <kjaakevin@gmail.com>
2015-05-25 21:56:39 -0430

    Eliminación del footer

--- upload.php
+++ upload.php
@@ -10,15 +10,20 @@ if(!isset($_SESSION['usuario'])){
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Subir Transferencias | Moldes y Matrices</title>
     <meta charset="utf-8"/>
-    <title>Subido</title>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/vendor/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/flat-ui.min.css"/>
+    <link rel="stylesheet" href="stylesheet.css"/>
 </head>
-<body>
+<body style="background:#e1e1e1">
 <?php
+require_once('nav.php');
 $subir = new Subir('nombreArchivo','archivoSubido');
 $subir->Upload();
 ?>
-
 <?php }?>
 <script src="js/vendor/jquery.min.js"></script>
 <script src="js/flat-ui.min.js"></script>
