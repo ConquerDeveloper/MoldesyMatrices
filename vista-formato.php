b1c779954c74661c146832d61503c00c3bbf9657
KEVIN <kjaakevin@gmail.com>
2015-05-25 21:52:49 -0430

    Vistas de Transferencias de Imágenes

---
+++ vista-formato.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3 class="panel-text text-center">
+                        <span class="fui-cross" style="font-size:80px; color: #e74c3c"></span> <br/>
+                        La imagen no cumple con los formatos de imagen permitidos.
+                    </h3>
+                    <p class="text-center panel-text">
+                        Lorem ipsum dolor sit amec Lorem ipsum dolor sit amec Lorem ipsum dolor sit amec
+                        Lorem ipsum dolor sit amec Lorem ipsum dolor sit amec Lorem ipsum dolor sit amec.
+                    </p>
+                    <a class="btn btn-primary btn-lg btn-block" href="<?php echo 'subir.php?id=' . $_SESSION['id_usuario'];?>">
+                        VOLVER
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ vista-subido.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3 class="panel-text text-center">
+                        <img src="img/icons/svg/retina.svg" alt="Retina"> <br/>
+                        Su imagen ha sido subida exitosamente.
+                    </h3>
+                    <p class="text-center panel-text">
+                        Lorem ipsum dolor sit amec Lorem ipsum dolor sit amec Lorem ipsum dolor sit amec.
+                        Lorem ipsum dolor sit amec Lorem ipsum dolor sit amec Lorem ipsum dolor sit amec.
+                    </p>
+                    <a class="btn btn-primary btn-lg btn-block" href="<?php echo 'inicio.php?id=' . $_SESSION['id_usuario'];?>">
+                        VOLVER
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
