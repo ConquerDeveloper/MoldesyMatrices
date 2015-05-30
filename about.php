6d41bf1b5f291d62a6a092e171e84b7e5b83539e
KEVIN <kjaakevin@gmail.com>
2015-05-30 08:41:19 -0430

    Sección de Nosotros

---
+++ about.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Acerca de | Moldes y Matrices</title>
+    <meta charset="utf-8"/>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/vendor/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/flat-ui.min.css"/>
+    <link rel="stylesheet" href="stylesheet.css"/>
+</head>
+<body>
+<?php include 'nav2.php';?>
+<div class="row">
+    <div class="col-md-4">
+        <div class="jumbotron">
+            <p>
+                Lorem ipsum pl0x :v
+            </p>
+        </div>
+    </div>
+    <div class="col-md-8">
+        <h3>saasd</h3>
+    </div>
+</div>
+<?php include 'footer2.php';?>
+</body>
+</html>
